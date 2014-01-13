de8659487266c1204df0b8c847821361eb0ef002
Stephen Yi-Hsien Lin <yihsien@princeton.edu>
2014-01-13 12:04:34 -0800

    encryption of safe_mail

--- safe_mail.py
+++ safe_mail.py
@@ -13,6 +13,14 @@ from safe_user import *
 #from email.MIMEMultipart import MIMEMultipart
 #from email.MIMEText import MIMEText
 from email.mime.text import MIMEText
+from keychain import *
+
+class mail(object):
+  def __init__(self, dev_id, body, key)
+    self.dev_id = dev_id
+    self.body = body
+    self.key = key
+
 class safe_mail(object):
 
   def send(self, message=False, receiver_addr=False, encrypt=False):
@@ -36,8 +44,28 @@ class safe_mail(object):
     else:
       body = message
     #Send the mail
-    
+
     msg = MIMEText(body)
+    
+    '''
+    if encrypt == True:
+      key=hashlib.sha256(random.randin(1,10000).digest()
+      iv = Random.new().read(AES.block_size)
+      obj=AES.new(key, AES.MODE_CFB, iv)
+      body = iv_obj.encrypt(body)
+      body = base64.encodestring(body)
+
+      s = SafeUser()
+      receiver_cert = None
+      encrypt_key = encrypt_with_cert(receiver_cert, key)
+      signed_key = sign_with_privkey(s.dev_kc.read_keychain()[1], encrypt_key)
+      signed_key = base64.encodestring(signed_key)     
+      
+      mail = mail(s.name+"."+s.dev.dev_name, body, signed_key)
+      dict_str = str(mail.__dict__)
+      msg = MIMEText(dict_str)
+    '''
+    #msg = MIMEText(body)
     #msg = MIMEMultipart(); 
     msg['Subject'] = "(Safe)-"+subject
     msg['From'] = account+"@gmail.com"
@@ -68,6 +96,9 @@ class safe_mail(object):
             if isinstance(response_part, tuple):
                 msg = email.message_from_string(response_part[1])
         
+        mail = mail(**msg)
+
+
         subject = msg['Subject']
         payload = msg.get_payload()
         if not safe_only:
